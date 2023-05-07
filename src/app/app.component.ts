b3185c1249972493b89b046a6da6b69ca1b10c9d
Tetiana Melnyk <uasmiykaua@gmail.com>
2023-05-07 12:35:14 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Todo } from './types/todo';
+import { TodosService } from './services/todos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  /* changeDetection: ChangeDetectionStrategy.OnPush, */
 })
-export class AppComponent {
-  title = 'angular_todo-app';
+
+export class AppComponent implements OnInit {
+  _todos: Todo[] = [];
+  activeTodos: Todo[] = [];
+
+  get todos() {
+    return this._todos;
+  }
+
+  set todos(todos: Todo[]) {
+    if (todos === this._todos) {
+      return;
+    }
+
+    this._todos = todos;
+    this.activeTodos = this._todos.filter(todo => !todo.completed);
+  }
+
+  constructor(
+    private todosService: TodosService,
+  ) {}
+
+
+  ngOnInit(): void {
+    this.todosService.todos$
+      .subscribe((todos) => {
+        this.todos = todos;
+      });
+  }
+
+  trackById(i: number, todo: Todo) {
+    return todo.id;
+  }
+
+  addTodo(newTitle: string) {
+    this.todosService.createTodo(newTitle)
+      .subscribe();
+  }
+
+  renameTodo(todo: Todo, title: string) {
+    this.todosService.updateTodo({ ...todo, title })
+      .subscribe();
+  }
+
+  toggleTodo(todo: Todo) {
+    this.todosService.updateTodo({...todo, completed: !todo.completed})
+      .subscribe();
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todosService.deleteTodo(todo)
+      .subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { FilterActivePipe } from './pipes/filter-active.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoFormComponent,
+    FilterActivePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
